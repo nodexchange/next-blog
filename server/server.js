e5a81a5cc626f9209ead6b9691b3d922435b773b
Martin <martin.wojtala@oath.com>
2019-02-19 22:13:21 +0000

    static wip

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const next = require('next')
+const { parse } = require('url')
+// const { join } = require('path')
+const path = require('path');
 
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev:dev, dir: './src' })
@@ -16,7 +19,17 @@ app.prepare()
   })
 
   server.get('*', (req, res) => {
-    return handle(req, res)
+    const parsedUrl = parse(req.url, true)
+    // TODO: (martin) make a dynamic list of static files
+    const rootStaticFiles = ['/robots.txt', '/sitemap.xml', '/favicon.ico']
+    if (rootStaticFiles.indexOf(parsedUrl.pathname) > -1) {
+      // const path = join(__dirname, 'static', parsedUrl.pathname)
+      console.log('file here?', path.resolve("./server/static/robots.txt"));
+      // app.serveStatic(req, res, path)
+      app.serveStatic(req, res, path.resolve("./server/static/robots.txt"));
+    } else {
+      handle(req, res, parsedUrl)
+    }
   })
 
   server.listen(3000, (err) => {
