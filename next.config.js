735a31d93c2ade921fef2ec1b3e673786e0e1edd
Martin (mac) <martin.wojtala@adtech.com>
2019-02-28 14:29:42 +0000

    images / asset loader added plus a new next-compose to import configs in a clean way

--- next.config.js
+++ next.config.js
@@ -1,20 +1,32 @@
 // next.config.js
 
 const withSass = require('@zeit/next-sass')
-module.exports = withSass({
+const withImages = require('next-images')
+const compose = require('next-compose')
+
+const imagesConfig = {/** ts config here */}
+const sassConfig = {
   cssModules: true,
   cssLoaderOptions: {
     importLoaders: 1,
     localIdentName: "[local]___[hash:base64:5]",
-  },
-  serverRuntimeConfig: {
-    // Will only be available on the server side
-    mySecret: 'secret'
-  },
-  publicRuntimeConfig: {
-    // Will be available on both server and client
-    // API_URL: process.env.API_URL
-    API_URL: process.env.API_URL
   }
-})
+}
+
+module.exports = compose([
+  [withImages, imagesConfig],
+  [withSass, sassConfig],
+  {
+    serverRuntimeConfig: {
+      // Will only be available on the server side
+      mySecret: 'secret'
+    },
+    publicRuntimeConfig: {
+      // Will be available on both server and client
+      // API_URL: process.env.API_URL
+      API_URL: process.env.API_URL
+    }
+  }
+])
+
 
