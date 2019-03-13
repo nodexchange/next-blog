8c127d6da15ae0c569fdad2d11311e59433f7a55
Martin (mac) <martin.wojtala@adtech.com>
2019-03-13 14:17:30 +0000

    fixed unmerged files

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,21 +1,10 @@
-<<<<<<< HEAD
 import { combineReducers } from 'redux'
-=======
-import { combineReducers } from 'redux-immutable'
-import addTodoReducer from '../containers/AddTodo/reducer'
->>>>>>> 9ffd86bbe1e82e73c59845f92bb7177c861c27d0
 import TimerReducer from '../containers/Timer/reducer'
 import TodoListReducer from '../containers/TodoList/reducer'
 
 const rootReducer = combineReducers({
-<<<<<<< HEAD
   timer: TimerReducer,
   todos: TodoListReducer
-=======
-  add: addTodoReducer(initialState),
-  // timer: TimerReducer(initialState),
-  todos: TodoListReducer(initialState)
->>>>>>> 9ffd86bbe1e82e73c59845f92bb7177c861c27d0
 });
 
 export default rootReducer
