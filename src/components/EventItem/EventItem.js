1e70d87950b3c2f7d84f428f6892521acb201ca3
Martin (mac) <martin.wojtala@adtech.com>
2019-03-17 18:43:38 +0000

    eventitem and list updated

--- src/components/EventItem/EventItem.js
+++ src/components/EventItem/EventItem.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from 'next/link'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemText from '@material-ui/core/ListItemText'
 import ListItemAvatar from '@material-ui/core/ListItemAvatar'
@@ -12,24 +13,30 @@ const styles = {
         overflow: 'hidden',
         textOverflow: 'ellipsis',
     },
+    activeLink: {
+        // cursor: 'pointer'
+    }
 }
 
 const EventItemView = ({ event, classes }) => (
     <ListItem>
+        
         <ListItemAvatar>
             <Avatar user={event.show.language} />
         </ListItemAvatar>
-        <ListItemText
-            primary={
-                <div className={classes.truncate}>
-                    <strong>
-                        {event.show.type ? event.show.type : 'Anonymous'}
-                    </strong>{' '}
-                    {event.show.name}
-                </div>
-            }
-            secondary={new Date(event.show.premiered).toLocaleString()}
-        />
+        <Link as={`/p/${event.show.id}`} href={`/post?id=${event.show.id}`}>
+            <ListItemText className={classes.activeLink}
+                primary={
+                    <div className={classes.truncate}>
+                        <strong>
+                            {event.show.type ? event.show.type : 'Anonymous'}
+                        </strong>{' '}
+                        {event.show.name}
+                    </div>
+                }
+                secondary={new Date(event.show.premiered).toLocaleString()}
+            />
+        </Link>
     </ListItem>
 )
 
--- src/components/EventList/EventList.js
+++ src/components/EventList/EventList.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import Link from 'next/link'
 import Card from '@material-ui/core/Card'
 import List from '@material-ui/core/List'
 import { withStyles } from '@material-ui/core/styles'
@@ -15,9 +14,7 @@ const EventListView = ({ events = [], classes }) => (
     <Card className={classes.root}>
         <List>
             {events.map(event => (
-                 <Link as={`/p/${event.show.id}`} href={`/post?id=${event.show.id}`}>
-                    <EventItem event={event} key={event.show.id} />
-                </Link>
+              <EventItem event={event} key={event.show.id} />
             ))}
         </List>
     </Card>
