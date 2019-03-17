408f1b7227b132447a887e00e1a87ece8f4d4443
Martin (mac) <martin.wojtala@adtech.com>
2019-03-17 17:25:27 +0000

    timeline storybook fixes (wip)

--- src/components/TimelineLoaded/TimelineLoaded.story.js
+++ src/components/TimelineLoaded/TimelineLoaded.story.js
@@ -6,7 +6,9 @@ const events = [
     {
         id: 1234,
         label: 'modified post "Hello World"',
-        createdAt: '2019-01-10T17:15:56.000Z',
+        show: {
+            premiered: '2019-01-10T17:15:56.000Z',
+        },
         author: {
             name: 'John Doe',
             email: 'jitewaboh@lagify.com',
@@ -15,7 +17,9 @@ const events = [
     {
         id: 1233,
         label: 'created new post "Hello World"',
-        createdAt: '2019-01-10T16:34:00.000Z',
+        show: {
+            premiered: '2019-01-10T16:34:00.000Z',
+        },
         author: {
             name: 'John Doe',
             email: 'jitewaboh@lagify.com',
@@ -25,7 +29,9 @@ const events = [
         id: 1232,
         label:
             'commented "I don\'t agree. You should never try to do things this way, or you\'ll end up in a bad place."',
-        createdAt: '2019-01-09T15:53:56.000Z',
+        show: {
+            premiered: '2019-01-09T15:53:56.000Z',
+        },
         author: {
             name: 'Lela Feng',
             email: 'lelafeng@example.com',
@@ -34,7 +40,9 @@ const events = [
     {
         id: 1231,
         label: 'deleted comment "Totally."',
-        createdAt: '2019-01-09T11:04:56.000Z',
+        show: {
+            premiered: '2019-01-09T11:04:56.000Z',
+        },
         author: {
             name: 'Brandon Hood',
             email: 'brandon@example.com',
