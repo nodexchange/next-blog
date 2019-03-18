b5793a8690f082779c40f59f212ed8f3c7a17e9d
Martin (mac) <martin.wojtala@adtech.com>
2019-03-18 23:19:03 +0000

    post

--- src/pages/post.js
+++ src/pages/post.js
@@ -1,15 +1,17 @@
-import { Layout } from '../components'
-import fetch from 'isomorphic-unfetch'
+import { Layout, MainPage } from "../components"
+import { TodoList } from "../containers"
+import fetch from "isomorphic-unfetch"
 
-const Post =  (props) => (
-    <Layout>
-       <h1>{props.show.name}</h1>
-       <p>{props.show.summary.replace(/<[\/]?p>/g, '')}</p>
-       <img src={props.show.image.medium}/>
-    </Layout>
+const Post = props => (
+  <Layout>
+    <h1>{props.show.name}</h1>
+    <p>{props.show.summary.replace(/<[\/]?p>/g, "")}</p>
+    <img src={props.show.image.medium} />
+    <MainPage {...props} />
+  </Layout>
 )
 
-Post.getInitialProps = async function (context) {
+Post.getInitialProps = async function(context) {
   const { id } = context.query
   const res = await fetch(`http://api.tvmaze.com/shows/${id}`)
   const show = await res.json()
